f00e1aa39499e0ddaa30070162e3e29ee1549787
Hamza Ayech <4yech.hamza@gmail.com>
2026-03-24 23:50:15 +0100

    feat: Show or hide the GoBack component in each page

--- web/app/components/PageWrapper.tsx
+++ web/app/components/PageWrapper.tsx
@@ -3,13 +3,15 @@ import GoBack from "./GoBack";
 export default function PageWrapper({
   children,
   title,
+  showGoBack = true,
 }: {
   children?: React.ReactNode;
   title?: string;
+  showGoBack?: boolean;
 }) {
   return (
     <div className="container mx-auto px-4 py-4">
-      <GoBack />
+      {showGoBack ? <GoBack /> : null}
       {title ? <h3 className="text-2xl font-bold mb-4">{title}</h3> : null}
       {children}
     </div>
