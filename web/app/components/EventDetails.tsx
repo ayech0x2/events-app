5833ac77a2ba7df655e2febc9803063c183343e5
Hamza Ayech <4yech.hamza@gmail.com>
2026-03-25 01:23:40 +0100

    feat: Adding EventDetails component

---
+++ web/app/components/EventDetails.tsx
@@ -0,0 +1,52 @@
+import { Event } from "@/app/types";
+import React from "react";
+import { useDate } from "../hooks/useDate";
+
+export function EventDetails({ id }: { id: string }) {
+  const [event, setEvent] = React.useState<Event | null>(null);
+
+  const [fetching, setFetching] = React.useState<boolean>(true);
+
+  const { formatDate } = useDate();
+
+  React.useEffect(() => {
+    fetch(`/api/events/${id}`)
+      .then((res) => res.json())
+      .then((data) => setEvent(data.event))
+      .finally(() => setFetching(false));
+  }, [id]);
+
+  if (fetching) {
+    return <p>Chargement...</p>;
+  }
+
+  return (
+    <div className="flex flex-col gap-4">
+      <DetailsRow label="Nom de l'événement" value={event?.title || ""} />
+      <DetailsRow label="Description" value={event?.description || ""} />
+      <DetailsRow
+        label="Date de début"
+        value={formatDate(event?.date || "")} // TODO: fix this
+      />
+      <div>
+        <span className="text-gray-500">
+          Inscriptions ({event?.registrations.length}):
+        </span>
+        <div className="font-semibold">
+          {event?.registrations
+            .map((registration) => registration.user.name)
+            .join(", ")}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const DetailsRow = ({ label, value }: { label: string; value: string }) => {
+  return (
+    <div className="flex flex-col gap-2">
+      <span className="text-gray-500">{label}:</span>
+      <span className="font-semibold">{value}</span>
+    </div>
+  );
+};
