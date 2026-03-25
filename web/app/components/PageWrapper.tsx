b361a1768b614d3bfc14f35d501226069baf6302
Hamza Ayech <4yech.hamza@gmail.com>
2026-03-25 01:19:50 +0100

    feat: Adding Actions to each PageWrapper

--- web/app/components/PageWrapper.tsx
+++ web/app/components/PageWrapper.tsx
@@ -4,15 +4,20 @@ export default function PageWrapper({
   children,
   title,
   showGoBack = true,
+  actions,
 }: {
   children?: React.ReactNode;
   title?: string;
   showGoBack?: boolean;
+  actions?: React.ReactNode;
 }) {
   return (
     <div className="container mx-auto px-4 py-4">
       {showGoBack ? <GoBack /> : null}
-      {title ? <h3 className="text-2xl font-bold mb-4">{title}</h3> : null}
+      <div className="flex flex-row justify-between items-center mb-4">
+        {title ? <h3 className="text-2xl font-bold">{title}</h3> : null}
+        {actions}
+      </div>
       {children}
     </div>
   );
