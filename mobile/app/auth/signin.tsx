5c5a38e9187501e51eaea8fbcc7ef1dde26f2342
Hamza Ayech <4yech.hamza@gmail.com>
2026-03-25 17:58:21 +0100

    refactor: Handling authentication with UI

--- mobile/app/auth/signin.tsx
+++ mobile/app/auth/signin.tsx
@@ -1,10 +1,27 @@
-import { ThemedText } from "@/components/themed-text";
-import { ThemedView } from "@/components/themed-view";
+import AppLogo from "@/components/AppLogo";
+import AuthFooter from "@/components/AuthFooter";
+import SigninForm from "@/components/forms/SigninForm";
+import { Box, Theme } from "@/restyle";
+import { useTheme } from "@shopify/restyle";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Signin() {
+  const { spacing } = useTheme<Theme>();
+
   return (
-    <ThemedView>
-      <ThemedText>Signin</ThemedText>
-    </ThemedView>
+    <Box flex={1} backgroundColor="bg">
+      <SafeAreaView
+        style={{
+          flex: 1,
+          paddingHorizontal: spacing.m,
+          gap: spacing.m,
+        }}
+      >
+        <AppLogo />
+        <Box marginTop="auto" />
+        <SigninForm />
+        <AuthFooter currentScreen="signin"  />
+      </SafeAreaView>
+    </Box>
   );
 }
--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,5 +1,16 @@
+import { useAuth } from "@/components/AuthProvider";
 import { Redirect } from "expo-router";
 
 export default function Index() {
+  const auth = useAuth();
+
+  if (auth?.loading) {
+    return null;
+  }
+
+  if (auth?.token) {
+    return <Redirect href="/application" />;
+  }
+
   return <Redirect href="/auth/signin" />;
 }
