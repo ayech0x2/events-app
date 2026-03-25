fbf4ca8cca5ff63c7e53db8a32fee1a41d26387b
Hamza Ayech <4yech.hamza@gmail.com>
2026-03-25 03:02:30 +0100

    feat: Mobile app initial structure

---
+++ mobile/app/auth/signin.tsx
@@ -0,0 +1,10 @@
+import { ThemedText } from "@/components/themed-text";
+import { ThemedView } from "@/components/themed-view";
+
+export default function Signin() {
+  return (
+    <ThemedView>
+      <ThemedText>Signin</ThemedText>
+    </ThemedView>
+  );
+}
