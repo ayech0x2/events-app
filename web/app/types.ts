38862d4ff0ba2865d43a71fc8cfba15fe74b976e
Hamza Ayech <4yech.hamza@gmail.com>
2026-03-25 01:22:03 +0100

    feat: Registration type added to types.ts

--- web/app/types.ts
+++ web/app/types.ts
@@ -4,11 +4,21 @@ interface CreateEventInput {
   date: string;
 }
 
+interface Registration {
+  id: number;
+  user: {
+    id: number;
+    name: string;
+    email: string;
+  };
+}
+
 interface Event {
   id: number;
   title: string;
   description: string;
   date: string;
+  registrations: Registration[];
 }
 
 export type { Event, CreateEventInput };
